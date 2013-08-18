e87d31124b33ed9e3685b7b8fbf687dc2d95a0bf
majklcz <6du1ro.n@gmail.com>
2013-08-18 10:35:34 +0200

    change gitignore and add mongo persistor

--- main.py
+++ main.py
@@ -10,12 +10,21 @@ config = {
 	"path": "files/"
 }
 
+mongo_config = {
+    "address": "persistor",
+    "host": "0.0.0.0",
+    "port": 8888,
+    "db_name": "test",
+    "pool_size": 20
+}
+
 def deploy_handler_web_server(err, dep_id):
     if err is not None:
         err.printStackTrace()
     else:
         print "web server been deployed! %s" %dep_id
 
-#vertx.deploy_module('io.vertx~mod-web-server~2.0.0-final', webconfig, 32, deploy_handler_web_server)
+vertx.deploy_module('io.vertx~mod-mongo-persistor~2.0.0-final', mongo_config, 1)
+
 vertx.deploy_verticle('server/upload.py', config)
 print config
