8d94409bf8b3246c9b74ff6fa03a4ce8e23dc3ef
majklcz <6du1ro.n@gmail.com>
2013-08-18 17:05:05 +0200

    add auth manager module

--- main.py
+++ main.py
@@ -14,12 +14,21 @@ config = {
     "path_symlink": "files/symlink/"
 }
 
+mongopersistor_address = "vertx.mongopersistor"
+
 mongo_config = {
-    "address": "vertx.mongopersistor",
+    "address": mongopersistor_address,
     "host": "0.0.0.0",
     "port": 8888,
     "db_name": "test",
     "pool_size": 20
+}
+
+auth_config = {
+    "address": "auth",
+    "user_collection": "users",
+    "persistor_address": mongopersistor_address,
+    "session_timeout": 900000
 }
 
 #called when deploy finish
@@ -39,6 +48,15 @@ def deploy_mongo(err, dep_id):
         logger.info("%s"% dep_id)
 
 vertx.deploy_module('io.vertx~mod-mongo-persistor~2.0.0-final', None, 1,handler=deploy_mongo)
+
+def deploy_auth(err, dep_id):
+    if err is not None:
+        err.printStackTrace()
+    else:
+        logger.info("Authorize Manager : %s" %dep_id)
+
+vertx.deploy_module('io.vertx~mod-auth-mgr~2.0.0-final', auth_config, 1,handler=deploy_auth)
+
 #vertx.deploy_module('io.vertx~mod-unzip~1.0.0-final', {"address":"unzip.module"}, 1,handler=deploy_handler)
 
 #main server / route matcher / eventbus
@@ -46,5 +64,6 @@ vertx.deploy_verticle('server/server.py', config, 1, handler=deploy_handler)
 
 logger.info("webserver config: %s"% config)
 logger.info("mongopersistor config: %s"% mongo_config)
+logger.info("webserver config: %s"% auth_config)
 #cleaner.periodic_cleaner(5000,"files/temp/",".*\.uploaded")
 #cleaner.periodic_cleaner(15000,"files/symlink/")
