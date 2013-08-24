686f7fee41266da0fcb719c50ac7302aea305789
Jakub Josef <jakub.josef@gmail.com>
2013-08-24 20:40:54 +0200

    First impl of local-storage + user_edit

--- web/js/app.js
+++ web/js/app.js
@@ -7,6 +7,7 @@ when('/', {controller:'UploadCtrl', templateUrl:'pages/upload.html'}).
 when('/search', {controller:'SearchCtrl', templateUrl:'pages/search.html'}).
 when('/login',{controller:'LoginCtrl',templateUrl:'pages/login.html'}).
 when('/registration',{controller:'LoginCtrl',templateUrl:'pages/registration.html'}).
+when('/account',{controller:'LoginCtrl',templateUrl:'pages/user_edit.html'}).
 when('/tests',{controller:'TestCtrl',templateUrl:'pages/tests.html'}).
 otherwise({redirectTo:'/'});
 });
--- web/js/controllers.js
+++ web/js/controllers.js
@@ -14,25 +14,36 @@ function UploadCtrl($scope,$eb){
 		registerFileUploader(params);
 	};
 }
-function LoginCtrl($scope,$rootScope,$location,$eb){
-	$scope.user={};
-	
+
+function LoginCtrl($scope,$rootScope,$location,$eb,localStorageService){
+	if(typeof $eb.userID !== 'undefined' && $eb.userID!==null){
+		$eb.send("get_user",{userID: $eb.userID},function(user){
+			$scope.$apply(function(){
+				$scope.user=user;
+			});
+		});
+	}else if(localStorageService.get("user")!=null){
+		var storageUser = localStorageService.get("user");
+		//login from localstorage
+		//$eb.login(storageUser.username,storageUser)
+		$scope.user=storageUser;
+		$rootScope.$broadcast("loggedIn",{user:storageUser});
+	}else{
+		$scope.user={};
+	}
 	$scope.doLogin= function(user){
-						//example call
+		//example call
 		$eb.send("user_exist_in_db",{username:user.login}, function(reply){
 			console.log(JSON.stringify(reply));
 		});
 		$eb.login(user.login,user.pass,function(res){
-			console.log(JSON.stringify(res));
+			$eb.send("get_user", { userID: $eb.userID},function(user){
+				$scope.user=user;
+				$rootScope.$broadcast('loggedIn',{user:$scope.user});
+				localStorageService.add('user',$scope.user);
+			});
 			console.log("sessionID: " + $eb.sessionID);
 			console.log("userID: " + $eb.userID);
-			//send event to all controllers
-			$rootScope.$broadcast('loggedIn');
-				user["_id"] = $eb.userID
-				user["username"] = "TEST UPDATE"
-				//$eb.send("registration",{user:user},function(res) {
-				//		console.log("user update");
-				//})
 			//minimal one file for result
 			$eb.send("mkdir_path",{"sessionID": $eb.sessionID,"name":"ahoj"}, function(res){
 				console.log(res);
@@ -45,16 +56,6 @@ function LoginCtrl($scope,$rootScope,$location,$eb){
 
 		alert(JSON.stringify("Logged in: "+user.login));
 	};
-	$scope.checkUsername = function(username){
-		$eb.send("user_exist_in_db",{username:username},function(res){
-			console.log(res);
-		});
-	}
-	$scope.checkEmail = function(email){
-		$eb.send("email_exist_in_db",{"email":email},function(res){
-			console.log(res);
-		});
-	}
 	$scope.doRegistration = function(user){
 
 		$eb.send("registration",{user:user},function(userID){
@@ -68,22 +69,36 @@ function LoginCtrl($scope,$rootScope,$location,$eb){
 			})
 		});
 	};
-
+	$scope.updateUser = function(user){
+		$eb.send("update_user",{user:user},function(userID){
+			//console.log(JSON.stringify(userID));
+			if(userID === user.id){
+				$location.path("upload");
+			}
+		});
+	};
+	$scope.checkUsername = function(username){
+		$eb.send("user_exist_in_db",{username:username},function(res){
+			console.log(res);
+		});
+	}
+	$scope.checkEmail = function(email){
+		$eb.send("email_exist_in_db",{"email":email},function(res){
+			console.log(res);
+		});
+	}
 }
+
 function HeaderCtrl($scope,$eb){
-	$scope.$on('loggedIn',function(){
-		$eb.send("get_user", { userID: $eb.userID},function(user){
-			//console.log(JSON.stringify(user));
-				$scope.user=user;
-		})
-		
+	$scope.$on('loggedIn',function(event,data){
+		$scope.user=data.user;
 	});
 	//TODO
 	$scope.doLogout=function(user){
 		$eb.login($eb.sessionID,function(res){
 			$rootScope.$broadcast('loggedOut');
 		});
-	}
+	};
 };
 
 function FooterCtrl($scope){
@@ -125,7 +140,7 @@ function SearchCtrl($scope, $eb){
 	$scope.publicSearch = function(search){
 		$scope.messages.initial=false;
 		//TODO check boxs for public private and both searchs for logged users
-		if ($eb.sessionID != null) {
+		if ($eb.sessionID !== null) {
 			$eb.send("simple_search",{"sessionID":$eb.sessionID,"matcher":{"filename": search.input, "type": "*"}}, function(reply){
 			console.log(JSON.stringify(reply.files));
 			setFiles(reply.files);
