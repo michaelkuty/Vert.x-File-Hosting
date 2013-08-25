fa90ce37892db920bcd535d363140756c1f25683
majklcz <6du1ro.n@gmail.com>
2013-08-25 23:36:50 +0200

    prepare for private download

--- server/file_service.py
+++ server/file_service.py
@@ -13,7 +13,7 @@ path_private = "files/private/"
 #TODO with private
 #only public files
 #content type headers
-def get_public_file(req):
+def get_public_file(req, path, uid, filename):
     def uid_exists(err, res):
         if not err:
             def mkdir(err, res):
@@ -23,26 +23,33 @@ def get_public_file(req):
 
                             req.response.status_code = 200
                             #req.response.put_header("Content-Type", "image/png")
-                            req.response.send_file(path_symlink + uid + "/" + filename)
+                            req.response.send_file(path + uid + "/" + filename)
                         else:
                             req.response.status_code = 200
-                            req.response.send_file(path_symlink + uid + "/" + filename) 
+                            req.response.send_file(path + uid + "/" + filename) 
 
-                    fs.link(path_symlink + uid + "/" + filename,path_public + uid + "/" + filename,handler=handle_symlink)
+                    fs.link(path + uid + "/" + filename,path + uid + "/" + filename,handler=handle_symlink)
                 else:
                     req.response.status_code = 200
-                    req.response.send_file(path_symlink + uid + "/" + filename) 
-            fs.mkdir(path_symlink + uid, handler = mkdir)
+                    req.response.send_file(path + uid + "/" + filename) 
+            fs.mkdir(path + uid, handler = mkdir)
         else:
             req.response.status_code = 404
             req.response.end("No such file or directory")
-    fs.exists(path_public + uid, handler = uid_exists)
+    fs.exists(path + uid, handler = uid_exists)
 
+#TODO remove params from call
 def file_handler(req):
     filename = req.params.get("filename", None)
     uid = req.params.get("uid", None)
     if (filename != None and uid != None):
-    	get_public_file(req)
+        def get_auth_uid(msg):
+            if (msg.body != None):
+                userID = uid.body
+                get_public_file(req, path_private, userID,filename)
+            else:
+        		get_public_file(req, path_public, uid,filename) 
+        EventBus.send("get_auth_uid", {"sessionID":uid}, get_auth_uid)
     else:
         req.response.status_code = 404
         req.response.end("missing params")
