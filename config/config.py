3d09bd377bef4328b550ae68aa46b2d66dea994b
majklcz <6du1ro.n@gmail.com>
2013-08-25 23:05:23 +0200

    complete config refactoring

---
+++ config/config.py
@@ -0,0 +1,89 @@
+"""
+GLOBAL config
+"""
+enviroments = [
+    {
+        "name": "production",
+        "enabled": True,
+        "port": 80,
+        "port_bridge": 8889,
+        "host": "http://master.majklk.cz",
+        "paths": {
+            "web": "web/",
+            "path_private":"files/private/",
+            "path_symlink":"files/symlink/",
+            "path_temp":"files/temp/",
+            "path_public":"files/public/"
+        },
+        "mongodb": {
+            "address": "vertx.mongopersistor",
+            "host": "localhost",
+            "port": 27017,
+            "db_name": "default_db",
+            "pool_size": 20,
+        },
+        "mailer": {
+            "address": "mailer",
+            "host": "smtp.googlemail.com",
+            "port": 465,
+            "ssl": True,
+            "auth": True,
+            "username": "kuty@koncepthk.cz",
+            "password": "MKmk+987",
+            "content-type": "text/html"
+        },
+        "files_collection": "files",
+        "users_collection": "users"
+    },
+    {
+        "name": "TEST",
+        "enabled": True,
+        "port": 8888,
+        "port_bridge": 8889,
+        "host": "0.0.0.0",
+        "paths": {
+            "web": "web/",
+            "path_private":"files/private/",
+            "path_symlink":"files/symlink/",
+            "path_temp":"files/temp/",
+            "path_public":"files/public/"
+        },
+        "mongodb": {
+            "address": "vertx.mongopersistor",
+            "host": "localhost",
+            "port": 27017,
+            "db_name": "default_db",
+            "pool_size": 20,
+        },
+        "mailer": {
+            "address": "mailer",
+            "host": "smtp.googlemail.com",
+            "port": 465,
+            "ssl": True,
+            "auth": True,
+            "username": "kuty@koncepthk.cz",
+            "password": "MKmk+987",
+            "content-type": "text/html"
+        },
+        "files_collection": "files",
+        "users_collection": "users"
+    }
+]
+mongopersistor_address = "vertx.mongopersistor"
+
+"""
+mongo db persistor 
+ligh vrstva EB || Mongo db server
+"""
+
+
+"""
+default
+zatim None
+"""
+auth = {
+    "address": "vertx.basicauthmanager.login",
+    "user_collection": "users",
+    "persistor_address": mongopersistor_address,
+    "session_timeout": 30000
+}
--- main.py
+++ main.py
@@ -1,29 +1,29 @@
 import vertx
 from server.utils import cleaner
-from config import config_test
-from config import mailer_prod
+from config import config
 from core.file_system import FileSystem
 fs = vertx.file_system()
 #get config for spec enviroment
-config = config_test
-
 logger = vertx.logger()
 
-logger.info("deploy vertx app in python start   ")
+logger.info("deploy filehosting app in python start   ")
 
 ###only create path from config
-paths = config.main.get("paths")
+for env in config.enviroments:
+    enabled = env.get("enabled",None)
+    if (enabled != None and enabled == True):
+        global_config = env
+        logger.info("USE %s CONFIG"% env.get("name"))
 
-def create_upload_dirs(paths):
-    for path in paths:
-        exist = fs.exists_sync(paths.get(path))
-        if not exist:
-            #logger.info("created %s")% paths.get(path)
-            fs.mkdir_with_parents(paths.get(path))
+        paths = env.get("paths")
+        def create_upload_dirs(paths):
+            for path in paths:
+                exist = fs.exists_sync(paths.get(path))
+                if not exist:
+                    #logger.info("created %s")% paths.get(path)
+                    fs.mkdir_with_parents(paths.get(path))
 
-create_upload_dirs(paths)
-
-
+        create_upload_dirs(paths)
 
 #TODO rewrite this module
 
@@ -32,17 +32,17 @@ def web_server_deploy(err, dep_id):
     if err is not None:
         err.printStackTrace()
     else:
-        logger.info("WEB-SERVER-OK on port: %s host: %s uid: %s"% (config.main.get("port",None),config.main.get("host",None),dep_id))
+        logger.info("WEB-SERVER-OK on port: %s host: %s uid: %s"% (global_config.get("port",None),global_config.get("host",None),dep_id))
 
-vertx.deploy_verticle('server/web_server.py', config.main, 1, handler=web_server_deploy)
+vertx.deploy_verticle('server/web_server.py', global_config, 1, handler=web_server_deploy)
 
 def sock_js_server_deploy(err, dep_id):
     if err is not None:
         err.printStackTrace()
     else:
-        logger.info("SOCK-JS-SERVER-OK on port: %s host: %s uid: %s"% (config.main.get("port_bridge",None),config.main.get("host",None),dep_id))
+        logger.info("SOCK-JS-SERVER-OK on port: %s host: %s uid: %s"% (global_config.get("port_bridge",None),global_config.get("host",None),dep_id))
 
-vertx.deploy_verticle('server/sock_js_server.py', config.main, 1, handler=sock_js_server_deploy)
+vertx.deploy_verticle('server/sock_js_server.py', global_config, 1, handler=sock_js_server_deploy)
 
 ##############################################MONGO#######################################################
 def deploy_mongo(err, dep_id):
@@ -57,7 +57,7 @@ def deploy_mongo(err, dep_id):
         vertx.deploy_verticle('server/utils/static_data.py', handler=static_handler)
         logger.info("MOD-MONGO-OK uid: %s"% dep_id)
 
-vertx.deploy_module('io.vertx~mod-mongo-persistor~2.0.0-final', config.mongo, 1,handler=deploy_mongo)
+vertx.deploy_module('io.vertx~mod-mongo-persistor~2.0.0-final', global_config.get("mongodb"), 1,handler=deploy_mongo)
 ##############################################AUTH#######################################################
 def deploy_auth(err, dep_id):
     if err is not None:
@@ -74,7 +74,7 @@ def deploy_mailer(err, dep_id):
     else:
         logger.info("MOD-MAILER uid: %s" %dep_id)
 
-vertx.deploy_module('io.vertx~mod-mailer~2.0.0-final', mailer_prod.config, 1,handler=deploy_mailer)
+vertx.deploy_module('io.vertx~mod-mailer~2.0.0-final', global_config.get("mailer"), 1,handler=deploy_mailer)
 ###########################################################################################################
 
 #vertx.deploy_module('io.vertx~mod-unzip~1.0.0-final', {"address":"unzip.module"}, 1,handler=deploy_handler)
