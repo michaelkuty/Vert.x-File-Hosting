5b14f49d67b9ba138c60ce9e882ca1471737a64a
Jakub Josef <jakub.josef@gmail.com>
2013-08-27 01:41:24 +0200

    Delete unused page and remove mess from app.js

--- web/js/app.js
+++ web/js/app.js
@@ -3,7 +3,6 @@ angular.module('filehosting', ['filehosting.services', 'filehosting.directives',
 config(function($routeProvider) {
 $routeProvider.
 when('/', {controller:'UploadCtrl', templateUrl:'pages/upload.html'}).
-//when('/edit/:projectId', {controller:EditCtrl, templateUrl:'detail.html'}).
 when('/search', {controller:'SearchCtrl', templateUrl:'pages/search.html'}).
 when('/login',{controller:'LoginCtrl',templateUrl:'pages/login.html'}).
 when('/registration',{controller:'LoginCtrl',templateUrl:'pages/registration.html'}).
