e28d96d2061ec07c76d7fbbb7a7de483f439fc8f
Jakub Josef <jakub.josef@gmail.com>
2013-08-26 18:42:53 +0200

    Fixed getting wrong protocol from get_hostname.

--- server/web_server.py
+++ server/web_server.py
@@ -108,7 +108,7 @@ EventBus.register_handler('mkdir_path', handler=mkdir_path)
 
 #def get_hostname
 def get_hostname(message):
-    message.reply("%s://%s"% ("htpp",app_config['host']))
+    message.reply("%s://%s"% ("http",app_config['host']))
 EventBus.register_handler('get_hostname',handler=get_hostname)
 
 def get_version(message):
