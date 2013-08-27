994a1cabae45d7f475ef20f73321ba426341caf7
Jakub Josef <jakub.josef@gmail.com>
2013-08-27 15:53:32 +0200

    Added function to format filesize in bytes to human-readable format.

--- web/js/controllers.js
+++ web/js/controllers.js
@@ -134,6 +134,13 @@ function UploadCtrl($scope,$eb){
 		registerFileUploader("/upload",params,callbacks);
 		$scope.uploaderInited=true;
 	};
+	$scope.bytesToSize = function (bytes) {
+	    var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+	    if (bytes == 0) return 'n/a';
+	    var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
+	    if (i == 0) return bytes + ' ' + sizes[i]; 
+	    return (bytes / Math.pow(1024, i)).toFixed(1) + ' ' + sizes[i];
+	};
 	if($eb.readyState()){
 		$scope.init_upload();
 	}else{
