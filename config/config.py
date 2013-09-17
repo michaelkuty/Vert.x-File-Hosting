a8bbc992117a3aa9a7a1c04f46af926c8fa8e840
michaelkuty <6du1ro.n@gmail.com>
2013-09-17 21:16:32 +0200

    rm

--- config/config.py
+++ config/config.py
@@ -28,8 +28,8 @@ enviroments = [
             "port": 465,
             "ssl": True,
             "auth": True,
-            "username": "kuty@koncepthk.cz",
-            "password": "MKmk+987",
+            "username": "",
+            "password": "",
             "content-type": "text/html"
         }
     },
@@ -62,8 +62,8 @@ enviroments = [
             "port": 465,
             "ssl": True,
             "auth": True,
-            "username": "kuty@koncepthk.cz",
-            "password": "MKmk+987",
+            "username": "",
+            "password": "",
             "content-type": "text/html"
         }
     }
