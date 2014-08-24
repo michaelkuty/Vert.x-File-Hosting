9bcd9d5baa7ebf7c3414ff69396e7b112b54a447
Michael Kuty <6du1ro.n@gmail.com>
2014-08-24 19:35:55 +0200

    cleanups config

--- mods/io.uhk~filehosting~0.0.9/server/config/config.py
+++ mods/io.uhk~filehosting~0.0.9/server/config/config.py
@@ -4,42 +4,8 @@ enviroments = [
         "enabled": True,
         "version": "0.9.0",
         "port": 80,
-        "port_bridge": 17000,
-        "host": "vertx.samsung.majklk.cz",
-        "paths": {
-            "web": "web/",
-            "path_private":"files/private/",
-            "path_symlink":"files/symlink/",
-            "path_temp":"files/temp/",
-            "path_public":"files/public/"
-        },
-        "mongodb": {
-            "address": "vertx.mongopersistor",
-            "host": "localhost",
-            "port": 27017,
-            "db_name": "default_db",
-            "pool_size": 20,
-        },
-        "files_collection": "files",
-        "users_collection": "users",
-        "mailer": {
-            "address": "mailer",
-            "host": "smtp.googlemail.com",
-            "port": 465,
-            "ssl": True,
-            "auth": True,
-            "username": "",
-            "password": "",
-            "content-type": "text/html"
-        }
-    },
-    {
-        "name": "TEST",
-        "enabled": False,
-        "version": "0.9.0-TEST-ENV",
-        "port": 8888,
         "port_bridge": 8889,
-        "host": "localhost",
+        "host": "0.0.0.0",
         "paths": {
             "web": "web/",
             "path_private":"files/private/",
